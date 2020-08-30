f86e88ae408cb48dade90dd09af19a824f0b1ee9
Nguyen <DucThang.Nguyen@emc.com>
2020-08-30 14:32:46 +0700

    2nd commit

---
+++ step3.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+GREEN = "#99CC99"
+
+COLOR = GREEN
+
+@app.route('/')
+def mainmenu():
+
+    response = """
+    <html>
+        <body bgcolor="{}">
+            <h1>Hello Cloud Native World</h1>
+            <br>
+            <a href="piper.html">Click Me!!!</a>
+        </body>
+    </html>
+    """.format(COLOR)
+
+    return response
+
+@app.route('/piper.html')
+def piper():
+
+    return """
+    <html>
+        <body>
+            <img src="static/piper.png">
+    
+        </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
